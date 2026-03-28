65583fb0379c46660136dc0a16ca71707f58210f
anaghate <anaghaghate3@gmail.com>
2026-03-28 00:38:23 -0400

    connection check added for mongoDB

--- db/mongo.py
+++ db/mongo.py
@@ -1,7 +1,14 @@
 from pymongo import MongoClient
 from config import Config
 
-client = MongoClient(Config.MONGO_URI)
+client = MongoClient(
+    Config.MONGO_URI,
+    serverSelectionTimeoutMS=10000
+)
+
+# Force a connection check on startup
+client.admin.command("ping")
+
 db = client[Config.DB_NAME]
 
 hotels_collection = db["hotels"]

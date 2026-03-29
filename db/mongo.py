29e09695dbaf80d528294fbf673ef91c3d93bbfe
anaghate <anaghaghate3@gmail.com>
2026-03-28 21:30:56 -0400

    dubugging completed - connection working with Render

--- db/mongo.py
+++ db/mongo.py
@@ -1,12 +1,6 @@
 from pymongo import MongoClient
 from config import Config
 import certifi
-import os
-import ssl
-
-print("MONGO_URI present:", bool(os.getenv("MONGO_URI")))
-print("DB_NAME:", os.getenv("DB_NAME"))
-print("OpenSSL version:", ssl.OPENSSL_VERSION)
 
 client = MongoClient(
     Config.MONGO_URI,
@@ -14,13 +8,6 @@ client = MongoClient(
     tlsCAFile=certifi.where(),
     serverSelectionTimeoutMS=10000
 )
-
-try:
-    client.admin.command("ping")
-    print("MongoDB ping successful")
-except Exception as e:
-    print("MongoDB ping failed:", str(e))
-    raise
 
 db = client[Config.DB_NAME]
 
